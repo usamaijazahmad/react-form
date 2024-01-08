3054055aae97eecd4b323bca86a90b65c7559b42
Usama Ijaz <usamaijaz2001@gmail.com>
2024-01-08 12:06:49 +0500

    commit

--- server/api.js
+++ server/api.js
@@ -1,5 +1,3 @@
-//const apiUrl = "https://fake-server-git-main-usama-ijazs-projects.vercel.app";
-//export const apiUrl = "http://localhost:3000";
 //export const apiUrl = "http://localhost:8080";
 
 export const apiUrl =
