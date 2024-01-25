457036ee76de68621c18acd85740d7af44f69b6d
Usama Ijaz <usamaijaz2001@gmail.com>
2024-01-25 10:56:23 +0500

    commit

--- server/api.js
+++ server/api.js
@@ -1,7 +1,7 @@
-export const apiUrl = "http://localhost:8080";
+// export const apiUrl = "http://localhost:8080";
 
-// export const apiUrl =
-//   "https://nodejs-practice-git-main-usama-ijazs-projects.vercel.app";
+export const apiUrl =
+  "https://nodejs-practice-git-main-usama-ijazs-projects.vercel.app";
 
 export const usersApiUrl = apiUrl + "/users";
 export const loginApiUrl = apiUrl + "/login";
